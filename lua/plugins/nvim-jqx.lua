9c9b688d07c96c059770ed79ee5ac4ef2177bcce
Daniel Serleg <daniel.serleg@marshmallow.co>
2025-09-22 21:36:13 +0200

    Json formatter and terraform support

--- lua/plugins/nvim-jqx.lua
+++ lua/plugins/nvim-jqx.lua
@@ -3,3 +3,12 @@ return {
   event = {"BufReadPost"},
   ft = { "json", "yaml" },
 }
+
+-- | Command    | description |
+-- | -------- | -------     |
+-- | JqxList  | populate the quickfix window with json keys    |
+-- | JqxList string | populate the quickfix window with string values     |
+-- | JqxQuery   | executes a generic jq query in the current file  |
+-- | <CR> | go to key location in file |
+-- | X | query values of key under cursor |
+-- | <Esc> | close floating window |
