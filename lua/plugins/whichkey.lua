decb4a83f2ad79758a57970ad0f0978c6dba1657
Daniel Serleg <daniel.serleg@marshmallow.co>
2025-10-08 16:43:45 +0200

    whichkey plugin fix

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -8,16 +8,26 @@ return {
         -- call the setup function with default properties
         which_key.setup()
 
-        -- Register prefixes for the different key mappings we have setup previously
-        which_key.register({
-            ['<leader>/'] = {name = "Comments", _ = 'which_key_ignore'},
-            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-            ['<leader>d'] = {name = '[D]ebug' , _ = 'which_key_ignore' },
-            ['<leader>e'] = {name = '[E]xplorer', _ = 'which_key_ignore'},
-            ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-            ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-            ['<leader>J'] = { name = '[J]ava', _ = 'which_key_ignore' },
-            ['<leader>w'] = {name = '[W]indow', _ = 'which_key_ignore'}
+        -- Register prefixes using the new spec format
+        which_key.add({
+            { "<leader>/", group = "Comments", icon = "" },
+            { "<leader>/_", hidden = true },
+            { "<leader>c", group = "[C]ode", icon = "" },
+            { "<leader>c_", hidden = true },
+            { "<leader>d", group = "[D]ebug", icon = "" },
+            { "<leader>d_", hidden = true },
+            { "<leader>e", group = "[E]xplorer", icon = "" },
+            { "<leader>e_", hidden = true },
+            { "<leader>f", group = "[F]ind", icon = "" },
+            { "<leader>f_", hidden = true },
+            { "<leader>g", group = "[G]it", icon = "" },
+            { "<leader>g_", hidden = true },
+            { "<leader>J", group = "[J]ava", icon = "" },
+            { "<leader>J_", hidden = true },
+            { "<leader>w", group = "[W]indow", icon = "" },
+            { "<leader>w_", hidden = true },
+            { "<leader>t", group = "[T]ab", icon = "" },
+            { "<leader>t_", hidden = true },
         })
     end
 }
